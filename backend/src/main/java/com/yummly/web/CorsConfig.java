c3b108cf6a78e3d5d77b3bb83d10a3de3c8df3a0
ImansiKavindi <kavindikulathunga203@gmail.com>
2025-05-10 19:30:29 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/yummly/web/CorsConfig.java
+++ backend/src/main/java/com/yummly/web/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Apply CORS for all endpoints
+                registry.addMapping("/**") 
                 .allowedOrigins(
                     "http://localhost:3000", 
                     "http://localhost:3001", 
